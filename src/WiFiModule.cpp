ec8deca519e43e151ad6176b114a2eb685c23fcd
Kevin Hou <strawberrypi64@gmail.com>
2024-08-18 13:45:18 +1000

    refactor: modularise wifi
    - WiFiModule.h
    - WiFiModule.cpp

---
+++ src/WiFiModule.cpp
@@ -0,0 +1,54 @@
+#include "WiFiModule.h"
+#include "WiFiSecrets.h"
+
+#include <WiFi.h>
+
+WiFiClient client;
+
+void WiFiModule::connect() {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    log_i("Connecting to %s", WIFI_SSID);
+
+    int attempts = 0;
+    while (WiFi.status() != WL_CONNECTED && attempts < 10) {
+        delay(1000);
+        log_i(".");
+        attempts++;
+    }
+    logConnectionStatus();
+}
+
+void WiFiModule::disconnect() {
+    log_i("Disconnecting from WiFi");
+    WiFi.disconnect(true);
+    WiFi.mode(WIFI_OFF);
+}
+
+bool WiFiModule::isConnected() {
+    return WiFi.status() == WL_CONNECTED;
+}
+
+void WiFiModule::reconnect() {
+    if (!isConnected()) {
+        log_i("Reconnecting to WiFi");
+        WiFi.reconnect();
+    }
+}
+
+void WiFiModule::setupAutoReconnect(bool enable) {
+    WiFi.setAutoReconnect(enable);
+    WiFi.persistent(enable);
+}
+
+String WiFiModule::getLocalIP() {
+    return WiFi.localIP().toString();
+}
+
+void WiFiModule::logConnectionStatus() {
+    if (isConnected()) {
+        log_e("WiFi Connected. IP Address: %s", getLocalIP().c_str());
+    } else {
+        log_e("Failed to connect to WiFi");
+    }
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <Arduino.h>
 
-#include <esp_bt.h>
 #include <esp_wifi.h>
-#include <WiFi.h>
+#include <esp_bt.h>
 
 #include <driver/adc.h>
 #include <driver/rtc_io.h>
@@ -12,9 +11,7 @@
 #include <base64.h>
 #include <string>
 
-#include "WiFiSecrets.h"
-
-WiFiClient client;
+#include "WiFiModule.h"
 
 /**
  * @brief Connects to WiFi using credentials from WiFiSecrets.h.
@@ -37,34 +34,16 @@ void setup() {
   btStop();
   esp_bt_controller_disable();
 
-  connectWiFi();
+  WiFiModule::connect();
+  WiFiModule::setupAutoReconnect(true);
+
 }
 
 void loop() {
-  delay(1000);
-  if (WiFi.status() != WL_CONNECTED) {
-    WiFi.reconnect();
-  }
-}
-
-void connectWiFi() {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  log_e("Connecting to %s", WIFI_SSID);
-
-  int attempts = 0;
-  while (WiFi.status() != WL_CONNECTED && attempts < 10) {
     delay(1000);
-    log_e(".");
-    attempts++;
-  }
-  if (WiFi.status() != WL_CONNECTED) {
-    log_e("Failed to connect to: %s", WIFI_SSID);
-    deepSleep();
-  } else {
-    log_e("WiFi Connected. IP Address: %s", WiFi.localIP().toString().c_str());
-    WiFi.setAutoReconnect(true);
-    WiFi.persistent(true);
-  }
+    if (!WiFiModule::isConnected()) {
+        WiFiModule::reconnect();
+    }
 }
 
 void deepSleep()  {
