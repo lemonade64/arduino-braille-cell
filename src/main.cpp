4526b221b8cdb7254f2737b8937434d6913b21b6
Kevin Hou <strawberrypi64@gmail.com>
2024-08-20 22:05:53 +1000

    chore: setup mcp23017

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <Arduino.h>
 
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFi.h>
 #include <esp_wifi.h>
 #include <esp_bt.h>
-
 #include <driver/adc.h>
 #include <driver/rtc_io.h>
 #include <soc/rtc_cntl_reg.h>
 #include <soc/soc.h>
-
 #include <string>
+
+#include <ArduinoJson.h>
+#include <Adafruit_MCP23X17.h>
 
 #include "WiFiModule.h"
 #include "CameraModule.h"
@@ -19,6 +23,8 @@
  * Disconnects WiFi, releases ADC power and starts deep sleep.
  */
 void deepSleep();
+
+Adafruit_MCP23X17 mcp;
 
 void setup() {
   Serial.begin(115200);
@@ -34,6 +40,9 @@ void setup() {
   std::string base64String = Camera::capturePhoto();
 
   log_e("Image Captured: %s", base64String.c_str());
+
+  const std::string ocrResult = "";
+  setupMCP23017();
 }
 
 void loop() {
@@ -51,4 +60,16 @@ void deepSleep()  {
   adc_power_release();
 
   esp_deep_sleep_start();
+}
+
+void setupMCP23017() {
+  Wire.begin(15,14);
+  if (!mcp.begin_I2C()) {
+    Serial.println("Error.");
+    while (1);
+  }
+
+  for (int i = 1; i < 6; i++) {
+    mcp.pinMode(i, OUTPUT);
+  }
 }
