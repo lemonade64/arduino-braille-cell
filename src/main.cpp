8989c838055752ef13e6329a8e48f3546819c5a4
Kevin Hou <strawberrypi64@gmail.com>
2024-08-22 21:59:40 +1000

    feat: power module

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,23 @@
 #include <Arduino.h>
 
-#include <HTTPClient.h>
-#include <WiFiClientSecure.h>
-#include <WiFi.h>
-#include <esp_wifi.h>
-#include <esp_bt.h>
-#include <driver/adc.h>
-#include <driver/rtc_io.h>
-#include <soc/rtc_cntl_reg.h>
-#include <soc/soc.h>
 #include <string>
 
+#include "PowerModule.h"
 #include "WiFiModule.h"
 #include "CameraModule.h"
 #include "OCRModule.h"
 #include "SolenoidModule.h"
 
-/**
- * @brief Puts the ESP32 into deep sleep mode.
- * 
- * Disconnects WiFi, releases ADC power and starts deep sleep.
- */
-void deepSleep();
-
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
-  setCpuFrequencyMhz(80);
-  btStop();
-  esp_bt_controller_disable();
+  PowerModule::init();
 
   WiFiModule::connect();
   WiFiModule::setupAutoReconnect(true);
 
   Camera::setup();
-  std::string base64String = Camera::capturePhoto();
+  const std::string base64String = Camera::capturePhoto();
 
   const std::string ocrResult = OCRModule::performOCR(base64String);
   
@@ -47,14 +30,4 @@ void loop() {
   if (!WiFiModule::isConnected()) {
     WiFiModule::reconnect();
   }
-}
-
-void deepSleep()  {
-  log_e("Entering Deep Sleep...");
-  WiFiModule::disconnect();
-  esp_wifi_stop();
-
-  adc_power_release();
-
-  esp_deep_sleep_start();
 }
