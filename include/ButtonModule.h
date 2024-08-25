2f97e6b1f7bfc81240ccc46d77ab07b7763b331f
Kevin Hou <strawberrypi64@gmail.com>
2024-08-26 00:31:34 +1000

    docs: button module

--- include/ButtonModule.h
+++ include/ButtonModule.h
@@ -3,13 +3,29 @@
 
 class ButtonModule {
 public:
-  static void init(int pin);
-  static bool isPressed();
-  static void update();
+    /**
+     * @brief Initialises the button with the correct pin.
+     * @param pin Arduino pin number to which the button is connected.
+     */
+    static void init(int pin);
+
+    /**
+     * @brief Checks if the button is currently pressed.
+     * @return true if the button is pressed, false otherwise.
+     */
+    static bool isPressed();
+
+    /**
+     * @brief Updates the current state of the button.
+     */
+    static void update();
 
 private:
-  static int buttonPin;
-  static int buttonState;
+    /// Arduino pin number for the button.
+    static int buttonPin;
+
+    /// Current state of the button (HIGH or LOW).
+    static int buttonState;
 };
 
 #endif
