4dd6a2fd48608bdff5855145596b4e4d6116d6b0
Kevin Hou <strawberrypi64@gmail.com>
2024-08-26 00:39:14 +1000

    done.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,30 @@
 #include "OCRModule.h"
 #include "SolenoidModule.h"
 #include "HapticModule.h"
+#include "ButtonModule.h"
+
+/**
+ * @brief Processes a captured image and converts it to Braille.
+ *
+ * @see Camera::capturePhoto()
+ * @see OCRModule::performOCR()
+ * @see HapticModule::playEffect()
+ * @see SolenoidModule::displayBraille()
+ */
+void processImageToBraille();
+
+void processImageToBraille() {
+  HapticModule::playEffect(HapticEffect::ON);
+
+  const std::string base64String = Camera::capturePhoto();
+
+  const std::string ocrResult = OCRModule::performOCR(base64String);
+  
+  HapticModule::playEffect(HapticEffect::SOLENOID_ON);
+  SolenoidModule::displayBraille(ocrResult);
+
+  HapticModule::playEffect(HapticEffect::OFF);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -18,6 +42,8 @@ void setup() {
   WiFiModule::setupAutoReconnect(true);
 
   HapticModule::init();
+
+  ButtonModule::init(2);
 
   Camera::setup();
   const std::string base64String = Camera::capturePhoto();
@@ -31,8 +57,14 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
   if (!WiFiModule::isConnected()) {
     WiFiModule::reconnect();
   }
+
+  ButtonModule::update();
+  if (ButtonModule::isPressed()) {
+    processImageToBraille();
+  }
+
+  delay(100);
 }
